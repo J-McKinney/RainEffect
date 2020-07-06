775531e2530d9f52d03d890b40fb65b068dca0fa
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-05 21:20:12 -0400

    yay

---
+++ src/RainEffect/RainEffect.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class RainEffect extends Component {
+  render() {
+    return (
+      <>
+        <div></div>
+      </>
+    );
+  }
+}
+
+export default RainEffect;
